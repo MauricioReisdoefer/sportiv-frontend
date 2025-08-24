f5e1ee83522ccd389c41fec106468a33e939b8dd
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-08-24 18:09:01 -0300

    REFACTOR: Creating register BLoC structure

--- lib/features/auth/presentation/bloc/login/login_bloc.dart
+++ lib/features/auth/presentation/bloc/login/login_bloc.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'login_event.dart';
 import 'login_state.dart';
@@ -19,14 +21,42 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
       emit(state.copyWith(password: event.password)); // Atualiza campo ".password"
     });
 
-    on<LoginSubmitted>((event, emit) {
+    on<LoginSubmitted>((event, emit) async {
       emit(state.copyWith(isSubmitting: true));
       debugPrint("------------------------");
       debugPrint("LOGIN SUBMITTED");
       debugPrint("------------------------");      
-      //
-      // TODO: chamar lógica de login ou API
-      //
+      
+      final apiUrl = dotenv.env['API_URL'];
+        final username = state.username;
+        final password = state.password;
+
+        try {
+          final response = await http.post(
+            Uri.parse('$apiUrl/login'), // ROTA DEFINIDA NO BACKEND
+            headers: {'Content-Type': 'application/json'},
+            body: '{"username": "$username", "password": "$password"}',
+          );
+
+          if (response.statusCode == 200) {
+            debugPrint("Login successful!");
+            // TO-DO: SALVAR TOKEN
+            // TO-DO: SALVAR TOKEN
+            // TO-DO: SALVAR TOKEN
+          } else {
+            debugPrint("Login failed: ${response.body}");
+            // TO-DO: EMITIR ERRO
+            // TO-DO: EMITIR ERRO
+            // TO-DO: EMITIR ERRO
+          }
+          } catch (e) {
+            debugPrint("Erro ao conectar: $e");
+            // TO-DO: EMITIR ERRO
+            // TO-DO: EMITIR ERRO
+            // TO-DO: EMITIR ERRO
+          }
+
+
       emit(state.copyWith(isSubmitting: false));
     });
   }
